d8073e7c0d052fc2a4d3aa578231c1ae38094fd5
Emily Hu <Smile0Hu55873@gmail.com>
2019-11-29 17:45:59 +0800

    add mistake

--- password1.py
+++ password1.py
@@ -1,13 +1,12 @@
 # Test password
 password = 'a123'
-p = 3
-while True:
+totalCount = 4
+i = totalCount
+while i > 0:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print ('登入成功!')
 		break #逃出迴圈
 	else:
-	   p = p - 1
-       print ('密碼錯誤!', p ,'次機會')
-       if p == 0:
-       	break
+		i = i - 1
+		print ('密碼錯誤', totalCount - i, '次')
